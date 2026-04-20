f99e15fa67cf16c622f7326e9bfe50bf42879fe8
ayush0821 <ayushjha0508@gmail.com>
2026-04-20 13:37:47 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -3,6 +3,11 @@
 # Get your FREE Groq API key from: https://console.groq.com
 # ─────────────────────────────────────────────────────────
 
-GROQ_API_KEY = "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"   # 🔑 Replace this!
+#GROQ_API_KEY = "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"   # 🔑 Replace this!
 
 COLLEGE_NAME = "Shoolini university"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
